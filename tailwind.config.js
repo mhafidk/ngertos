9a66f242a30a91137ca54359f4d46f0941caacce
mhafidk <mhafidk@gmail.com>
2024-11-02 16:46:24 +0700

    add header and footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {
+			fontFamily: {
+				sans: ['Outfit']
+			}
+		}
 	},
 
 	plugins: [typography, forms]
