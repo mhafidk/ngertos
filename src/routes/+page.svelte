96aa9bdd58c896e528abd1e1ac3e1486151616b8
mhafidk <mhafidk@gmail.com>
2024-11-02 18:49:38 +0700

    add search bar

---
+++ src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<div class="py-8">
+	<h1 class="text-center text-2xl font-semibold">
+		Mau <span class="text-lime-600">belajar</span> apa hari ini?
+	</h1>
+	<div class="mt-4 flex w-full items-center">
+		<input
+			type="text"
+			class="form-input h-10 w-10/12 rounded-l border-transparent bg-lime-50 px-4 py-2 focus:border-lime-600 focus:bg-white focus:ring-0 md:w-11/12"
+		/>
+		<div
+			class="flex h-10 w-2/12 items-center justify-center rounded-r border border-lime-600 bg-lime-600 hover:cursor-pointer hover:border-lime-700 hover:bg-lime-700 md:w-1/12"
+		>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				height="34px"
+				viewBox="0 -960 960 960"
+				width="34px"
+				fill="#ffffff"
+				class=""
+				><path
+					d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z"
+				/></svg
+			>
+		</div>
+	</div>
+</div>
